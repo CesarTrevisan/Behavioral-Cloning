6ea1272429792b3e78d721df70afdd05425677e2
César Trevisan <trevisan.cesar@gmail.com>
2017-07-15 08:15:42 -0300

    Update model.py

--- model.py
+++ model.py
@@ -15,9 +15,9 @@ from keras.layers.pooling import MaxPooling2D
 # Importing csv with features and labels
 lines = []
 with open('data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
+	reader = csv.reader(csvfile)
+	for line in reader:
+		lines.append(line)
 
 #define length
 length=len(lines)
